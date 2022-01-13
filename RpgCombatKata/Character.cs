4577453ee628307a3de22c7b1a52737420978b53
Palisar <Palisar@hotmail.co.uk>
2022-01-13 14:13:21 +0000

    added constructor to take a name , Name property and  a ToString

--- RpgCombatKata/Character.cs
+++ RpgCombatKata/Character.cs
@@ -8,9 +8,21 @@ namespace RpgCombatKata
 {
     public class Character
     {
+        public Character(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; init; }
         public int MaxHP { get; set; } = 1000;
         public int Health { get; set; } = 1000;
         public int Level { get; set; } = 1;
         public bool IsAlive { get; set; } = true;
+
+        public override string ToString()
+        {
+            return $@"{Name}
+Level : {Level}
+HP : {Health}/{MaxHP}";
+        }
     }
 }
