cfca95ae7ff460791699cecebc0b1ec9c49889f4
Palisar <Palisar@hotmail.co.uk>
2022-01-14 11:18:35 +0000

    added  a rules engine to simplify adding new combat features in the future

---
+++ RpgCombatKata/Engines/Combat/BaseCombatRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgCombatKata.Engines.Combat
+{
+    public abstract class BaseCombatRule
+    {
+        protected readonly CharacterProxy attacker;
+        protected readonly CharacterProxy target;
+
+        public BaseCombatRule(CharacterProxy attacker, CharacterProxy target)
+        {
+            this.attacker = attacker;
+            this.target = target;
+        }
+        public abstract bool CanReach();
+        public void MakeAttack(int dmg)
+        {
+            attacker.Attack(target, dmg, attacker);
+        }
+        public abstract bool IsInRangeCheck();
+    }
+}
---
+++ RpgCombatKata/Engines/Combat/MeleeCombatRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgCombatKata.Engines.Combat
+{
+    public class MeleeCombatRule : BaseCombatRule
+    {
+        public MeleeCombatRule(CharacterProxy attacker, CharacterProxy target) : base(attacker, target)
+        {
+        }
+        public override bool CanReach()
+        {
+            return base.attacker.Type == CombatType.Melee && IsInRangeCheck();
+        }
+
+        public override bool IsInRangeCheck()
+        {
+            //formula finds the distance between 2 points 
+            var squareXs = Math.Abs(Math.Pow((base.attacker.Position.X - base.target.Position.X), 2));
+            var squareYs = Math.Abs(Math.Pow((base.attacker.Position.Y - base.target.Position.Y), 2));
+            return Math.Sqrt(squareYs + squareXs) <= 2;
+        }
+    }
+}
