ad3c08e8428dc6d68d3d86bec3578d5809a3f175
Palisar <Palisar@hotmail.co.uk>
2022-01-14 11:23:34 +0000

    added data file section to populate with data collections in order to mock a database

---
+++ RpgCombatKata.Data/InMemoryEnemyDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgCombatKata.Data
+{
+    public class InMemoryEnemyDatabase
+    {
+        List<Character> Enemies = new List<Character>();
+
+        //TODO : Add logic to create a list of enemies for an encounter
+    }
+}
