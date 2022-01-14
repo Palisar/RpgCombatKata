455d47e4345008493ee4cd8085e9a9ca4e6de490
Palisar <Palisar@hotmail.co.uk>
2022-01-14 11:21:40 +0000

    added a file for the structs so far it only contains a Position struct but could grow in time and may need to split into seperate fiels and create a structs folder

---
+++ RpgCombatKata/structs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgCombatKata
+{
+    public struct Position
+    {
+        //distance to be measured in meters
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
