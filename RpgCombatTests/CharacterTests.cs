5077b6d7a44d26ee765c2d4dda180276525e5ba2
Palisar <Palisar@hotmail.co.uk>
2022-01-14 11:24:12 +0000

    fixed the test to work with updated character model

--- RpgCombatTests/CharacterTests.cs
+++ RpgCombatTests/CharacterTests.cs
@@ -5,13 +5,14 @@ namespace RpgCombatTests
         [Fact]
         public void CreateCharacter()
         {
-            var character = new Character("Cloud");
+            var character = new Character("Cloud", CombatType.Melee);
             var proxy = new CharacterProxy(character);
             proxy.Name.Should().Be(character.Name);
-            proxy.Health.Should().Be(character.Health);
+            proxy.HP.Should().Be(character.HP);
             proxy.Level.Should().Be(character.Level);
             proxy.IsAlive.Should().Be(character.IsAlive);
             proxy.MaxHP.Should().Be(character.MaxHP);
         }
+
     }
 }
