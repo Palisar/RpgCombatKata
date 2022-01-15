86a56c6e4f1e6703aecfa8e379f4209872b2ee30
Paul Keating <palisar@hotmail.co.uk>
2022-01-15 19:45:06 +0000

    Added BaseFaction class  which can be implimented in the future along with an enum for the different factions,

---
+++ RpgCombatKata/Factions/BaseFaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgCombatKata
+{
+    public enum Factions
+    {
+        Warfarts,
+        Pizzers
+    }
+    public abstract class BaseFaction
+    {
+        public Factions Type { get; set; }
+        public string? Name { get; set; }
+        public List<Character>? Members { get; set; }
+
+        public abstract void AddMembers();
+        public abstract void RemoveMembers();
+    }
+}
