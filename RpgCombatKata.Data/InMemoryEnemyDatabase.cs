3dc01d5809e752c9e7fd09ff0ca2b6259919f9fb
Paul Keating <palisar@hotmail.co.uk>
2022-01-16 20:01:52 +0000

    code cleanup still need to impliment this for testing purposes

--- RpgCombatKata.Data/InMemoryEnemyDatabase.cs
+++ RpgCombatKata.Data/InMemoryEnemyDatabase.cs
@@ -8,7 +8,7 @@ namespace RpgCombatKata.Data
 {
     public class InMemoryEnemyDatabase
     {
-        List<Character> Enemies = new List<Character>();
+        private readonly List<Character> Enemies = new ();
 
         //TODO : Add logic to create a list of enemies for an encounter
     }
