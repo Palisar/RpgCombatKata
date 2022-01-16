36f17a0efc0778a552bb3ae382a4de94ccfe2e3c
Paul Keating <palisar@hotmail.co.uk>
2022-01-16 20:00:27 +0000

    Added an GameObject class that can be targeted

---
+++ RpgCombatKata/Objects/GameObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgCombatKata
+{
+    public class GameObject : ITakeDamage, IHealthBar
+    {
+        public GameObject(string name, int maxHP)
+        {
+            Name = name;
+            MaxHP = maxHP;
+            HP = maxHP;
+        }
+        public string Name { get; set; }
+        public int MaxHP { get; set; }
+        public int HP { get; set; }
+        public bool IsAlive { get; set; } = true;
+        public void TakeDamage(int dmg)
+        {
+            HP -= dmg;
+            if (HP <= 0)
+            {
+                IsAlive = false;
+            }
+        }
+    }
+}
