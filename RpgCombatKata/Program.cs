c55cd051e41b045f90d4516dd2e48eec0bc0dc1c
Paul Keating <palisar@hotmail.co.uk>
2022-01-19 18:32:47 +0000

    House keeping

--- RpgCombatKata/Program.cs
+++ RpgCombatKata/Program.cs
@@ -1,2 +1,3 @@
-﻿Console.WriteLine("You stroll through the forest on a nice summers day...");
+﻿Console.WriteLine("You stroll through the forest on a nice summers day\nwhen suddenly a group bandits ambush you out of nowhere!");
+
 Console.ReadLine();
--- RpgCombatKata/usings.cs
+++ RpgCombatKata/usings.cs
@@ -1,2 +1,3 @@
 ﻿global using RpgCombatKata.Interfaces;
 global using RpgCombatKata.Engines.Combat;
+global using RpgCombatKata.Objects;
